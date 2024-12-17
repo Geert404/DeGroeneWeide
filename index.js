bc6a002b2070e1acd97a2586b99e6a48ad76f04f
Benenlux <simvee2000@gmail.com>
2024-12-17 09:14:21 +0100

    Updated website

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
-/*
-const sheet = CSSStyleSheet();
 
-class ingredientKaart extends HTMLElement {
-  constructor(){
-    super();
+async function test(){
+  const get = fetch("http://localhost:8080/api/products", {
+    "method" : 'GET',
+  });
 
-    const shadowRoot = this.attachShadow({mode: 'open'});
-    let h2 = document.createElement("h2");
-    h2.classList.add("Titel");
-    h2.part.add("Titel")
-    h2.textContent = "Helooooooooooo";
-    shadowRoot.appendChild(h2);
-  }
+  console.log((await get));
+  const get_result = (await get).json();
+  console.log((await get_result));
 }
 
-customElements.define("ingredient-kaart", ingredientKaart);
-*/
+test();
 
-const parentdiv = document.getElementById("parent");
-
-/*
-window.onload = function() {
-  const ingredient_1 = document.createElement("p");
-  ingredient_1.innerHTML = `
-    <div class= "ingredient-container">
-          <img class="ingredient-foto" src="images/Placeholder.jpg">
-          <!--Bestelknop met titel-->
-          <div class="bestel-knop-en-titel">
-            <p>Wagyu</p>
-            <!-- Bestel knop-->
-            <div class="bestel-knop-container-small">
-              <img class="knop-icon-small" src="Icons/Plus-icon.svg">
-            </div>
-          </div>
-          <!--Prijs en aantal-->
-          <div>
-            <p class="prijs">4.<span class="prijs-cent">99</span></p>
-          </div>
+const ingredient_1 = document.createElement("p");
+ingredient_1.innerHTML = `
+  <div class= "ingredient-container" id="kaart">
+      <div class="ingredient-foto-container">
+        <img class="ingredient-foto" src="images/Placeholder.jpg">
+        <!--Prijs en aantal-->
+        <div class="prijs-en-aantal">
+          <p class="prijs">4.<span class="prijs-cent">99</span></p>
+          <p class="hoeveelheid">100g</p>
         </div>
-  `;
-  document.body.appendChild(ingredient_1)
-}
+        
+      </div >
+      <!--Bestelknop met titel-->
+      <div class="bestel-knop-en-titel">
+        <p>Wagyu</p>
+        <!-- Bestel knop-->
+        <div class="bestel-knop-container-small">
+          <img src="Icons/Plus-icon.svg">
+        </div>
+      </div>
+      <div class="uitklap-en-voorraad">
+        <div class="uitklap-container">
+          <img class="uitklap-icon" src="Icons/Expand-icon.svg">
+        </div>
+        <p class="voorraad">voorraad: <span>15</span></p>
+      </div>
 
-*/
+    </div>
+`;
+  document.body.appendChild(ingredient_1);
+  let button = document.getElementById("button");
+  console.log(button);
+  button.onclick = function() {
+    let ingredienten_kaart = document.getElementById("kaart")
+    ingredienten_kaart.className="ingredient-container-uitgeklapt";  
+    console.log("button clicked");
+  };
+
 customElements.define(
   "ingredient-kaart",
   class extends HTMLElement {
@@ -56,3 +62,5 @@ customElements.define(
     }
   },
 );
+
+
